c6616a85354150f8d50abc2645b76a81ab73355e
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-21 14:15:58 -0600

    added routes to app.js

--- salty-hn/src/App.js
+++ salty-hn/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import FormikUserLogin from './components/UserLogin';
+import FormikUserRegistration from './components/UserRegister';
 
 function App() {
   return (
     <div className="App">
       <p>Salty Hackers</p>
+      <Route path="/components/UserLogin" render={props => <FormikUserLogin {...props} />} />
+      <Route path="/components/UserRegister" render={props => <FormikUserRegistration {...props} />} />
     </div>
   );
 }
