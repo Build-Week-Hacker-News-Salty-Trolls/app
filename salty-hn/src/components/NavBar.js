55592a53548e60a23565e1e9301ad4eba0e0f781
Tony <tonyrkovar@gmail.com>
2019-10-23 17:04:34 -0500

    Testing continuous deployment

--- salty-hn/src/components/NavBar.js
+++ salty-hn/src/components/NavBar.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { NavBarStyled } from './Styling';
 
 const NavBar = () => {
-
     return (
         <NavBarStyled>
-            <p>Your UserName</p>
+            <p>OUR UserName</p>
             <a href='/home'>Log Out</a>
             <a href='/saved-comments'>Saved Comments</a>
             <a href='/comments'>Search</a>
